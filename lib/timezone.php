ca33a6d42e6ed4a9d2ed59e71a03e05508264e80
adlawson <adlawson@gmail.com>
2013-08-16 13:21:55 +0100

    Add source

---
+++ lib/timezone.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see  http://github.com/adlawson/timezone/blob/master/LICENSE
+ * @link http://github.com/adlawson/timezone
+ */
+
+if ('' === ini_get('date.timezone')) {
+    date_default_timezone_set('UTC');
+}
